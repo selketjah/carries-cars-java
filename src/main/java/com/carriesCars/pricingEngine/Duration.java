214dcbb78f3b054fe6b4b02b1ac8fdc3959e0e14
Marijn Huizendveld <marijn@huizendveld.com>
2022-07-04 02:03:41 +0200

    :white_check_mark: `Duration_is_at_least_one_minute` and `Converts_from_and_to_text`

--- src/main/java/com/carriesCars/pricingEngine/Duration.java
+++ src/main/java/com/carriesCars/pricingEngine/Duration.java
@@ -1,16 +1,41 @@
 package com.carriesCars.pricingEngine;
 
+import java.util.Objects;
+
 public class Duration {
+    private final int durationInMinutes;
+
+    public Duration(int durationInMinutes) {
+        this.durationInMinutes = durationInMinutes;
+    }
+
     public static Duration ofMinutes(int length) {
-        return null;
+        if (length < 1) {
+            throw new IllegalArgumentException("Sorry, Duration should be at least one minute.");
+        }
+
+        return new Duration(length);
     }
 
     public static Duration fromString(String textRepresentation) {
-        return null;
+        return Duration.ofMinutes(Integer.parseInt(textRepresentation));
     }
 
     @Override
     public String toString() {
-        return "Duration{}";
+        return String.format("%d", this.durationInMinutes);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Duration duration = (Duration) o;
+        return durationInMinutes == duration.durationInMinutes;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(durationInMinutes);
     }
 }
